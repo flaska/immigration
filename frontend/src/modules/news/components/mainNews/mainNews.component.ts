e98448b49c491637d1831ad4013f795ed99d21ab
Jakub Flaska <jakub.flaska@nih.gov>
2018-07-05 16:57:01 -0400

    x

--- frontend/src/modules/news/components/mainNews/mainNews.component.ts
+++ frontend/src/modules/news/components/mainNews/mainNews.component.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({selector: 'main-news', templateUrl: './mainNews.component.html'})
 export class MainNewsComponent{
-  channel = 'H1B';
+  channel = 'green card';
   changed(v){
     this.channel = v;
   }
--- server/newsFetchService.js
+++ server/newsFetchService.js
@@ -6,16 +6,27 @@ exports.searchByTerm = function(term, cb){
     .get('https://news.google.com/news?output=rss&q=' + term, function(err, resp, body){
       parseString(body, function (err, result) {
         var feeds = result.rss.channel[0].item.map(r=>{
-          return {title: r.title[0], url: r.link[0], date: r.pubDate[0]};
+          return {title: r.title[0], url: r.link[0], date: r.pubDate[0], img: r.description[0]};
         });
         feeds.shift();
         feeds = feeds.map(f=>{
           var re = /\&url\=.+$/;
           var found = f.url.match(re)[0].substr(5,1000);
-
           f.url = found;
           return f;
         });
+
+        feeds = feeds.map(f=>{
+          var re = /img src=".+?"/;
+          try {
+            var found = f.img.match(re)[0].substr(9, 1000);
+            f.img = found.substr(0, found.length - 1);
+          } catch(e) {
+            console.log(e);
+          }
+          return f?f:'';
+        });
+
         cb(err, feeds);
       });
     });
