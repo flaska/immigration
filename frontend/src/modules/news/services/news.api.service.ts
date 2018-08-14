1a8101beb2a08b881c3b49ac567bffb7aa8c7280
Jakub Flaska <jakub.flaska@nih.gov>
2018-08-14 16:45:21 -0400

    x

--- frontend/src/modules/news/services/news.api.service.ts
+++ frontend/src/modules/news/services/news.api.service.ts
@@ -3,7 +3,7 @@ import {HttpClient} from '@angular/common/http';
 import {NewsRecord} from '../schemas/news.record.schema';
 import {Observable} from 'rxjs';
 import {APP_BASE_HREF} from '@angular/common';
-import {TransferState, StateKey, makeStateKey} from '@angular/platform-browser';
+import {TransferState, makeStateKey} from '@angular/platform-browser';
 import {retry} from 'rxjs/operators';
 
 export class BlockedUrl{
@@ -22,35 +22,22 @@ export class NewsApiService {
     console.log('server url: ' + this.serverUrl)
   }
 
-
   private serverUrl = '/';
-
   private getUrl: string = 'api/getNews';
   private blockAddr: string = 'api/blockUrl';
   private blockedFeedsAddr: string = 'api/getBlockedUrls';
 
-
   searchByTerm(term: string, scoring: string, from: number, cb: (error:any, result:NewsRecord[])=>void){
-
-    console.log('search by term');
 
     var cachedState = this.transferState.get(makeStateKey(term+scoring+from), null as NewsRecord[]);
     if (cachedState) return cb(null, cachedState);
 
-    console.log('server querying: ' + this.serverUrl + this.getUrl + '?q=' + term + '&from=' + from + '&scoring=' + scoring);
-
     this.http.get<NewsRecord[]>(this.serverUrl + this.getUrl + '?q=' + term + '&from=' + from + '&scoring=' + scoring).pipe(retry(3)).subscribe((result)=>{
-      console.log('query succsess');
-      console.log(result[0]);
-      console.log('storing state ' + makeStateKey(term+scoring+from));
       this.transferState.set(makeStateKey(term+scoring+from), result as NewsRecord[]);
       cb(null, result);
     },error => {
-      console.log('query error');
-      console.log(error);
       cb(error, null);
     });
-
   }
 
   blockUrl(url: string, password: string):Observable<boolean>{
--- index.js
+++ index.js
@@ -2,20 +2,17 @@ const express = require('express'),
   compression = require('compression'),
   app = express(),
   routes = require('./server/routes'),
-  request = require('request'),
-  sslRedirect = require('heroku-ssl-redirect'),
   path = require('path'),
   bodyParser = require('body-parser')
 ;
-// app.use(sslRedirect());
 app.use(compression());
 app.use(bodyParser.json());
-app.use(express.static('./frontend/dist/browser'));
+// app.use(express.static('./frontend/dist/browser'));
 app.use('/api', routes);
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './frontend/dist/browser/index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, './frontend/dist/browser/index.html'));
+// });
 
 const webServerPort = 3001;
 
