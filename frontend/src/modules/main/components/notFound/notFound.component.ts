fd5764d8f6e412000c22b16f13de2c725dd1adbf
Jakub Flaska <jakub.flaska@nih.gov>
2018-08-17 14:01:05 -0400

    x

---
+++ frontend/src/modules/main/components/notFound/notFound.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'not-found',
+  templateUrl: './notFound.component.html',
+  styleUrls: ['./notFound.component.scss']
+})
+export class NotFoundComponent {
+}
