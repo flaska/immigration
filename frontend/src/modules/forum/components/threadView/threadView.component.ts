df4f7308d04800226a7acab83d10ecdb9e12550c
Jakub Flaska <jakub.flaska@nih.gov>
2018-08-22 16:35:00 -0400

    x

--- frontend/src/modules/forum/components/threadView/threadView.component.ts
+++ frontend/src/modules/forum/components/threadView/threadView.component.ts
@@ -2,24 +2,27 @@ import {Component, Input, OnInit} from '@angular/core';
 import {ForumApiService} from '../../services/forum.api.service';
 import {Observable} from 'rxjs';
 import {ForumPost, ForumThread} from '../../schemas/forum.schemas';
+import {ActivatedRoute} from '@angular/router';
 
 
 @Component({
-  selector: 'thread-list',
-  templateUrl: './threadList.component.html',
-  styleUrls: ['./threadList.component.scss']
+  selector: 'thread-view',
+  templateUrl: './threadView.component.html',
+  styleUrls: ['./threadView.component.scss']
 })
 export class ThreadViewComponent implements OnInit{
 
-  @Input() threadId: string;
+  threadId: string;
   postList: Observable<ForumPost>;
   thread: Observable<ForumThread>;
 
-  constructor(private forumService: ForumApiService){
+  constructor(private forumService: ForumApiService, private route: ActivatedRoute){
 
   }
 
   ngOnInit(){
+    this.threadId = this.route.snapshot.paramMap.get('threadId');
+    console.log(this.threadId);
   }
 
 }
