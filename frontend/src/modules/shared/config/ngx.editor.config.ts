b35fb3e1e6381fd7506188c45ff322604cecd30a
Jakub Flaska <jakub.flaska@nih.gov>
2018-08-29 16:14:17 -0400

    'x'

--- frontend/src/modules/shared/config/ngx.editor.config.ts
+++ frontend/src/modules/shared/config/ngx.editor.config.ts
@@ -1,9 +1,21 @@
+/*
+
+[
+    ["bold", "italic", "underline", "strikeThrough", "superscript", "subscript"],
+    ["fontSize", "color"],
+    ["justifyLeft", "justifyCenter", "justifyRight", "justifyFull", "indent", "outdent"],
+    ["cut", "copy", "delete", "removeFormat", "undo", "redo"],
+    ["paragraph", "blockquote", "removeBlockquote", "horizontalLine", "orderedList", "unorderedList"],
+    ["link", "unlink", "image"]
+]
+* */
+
 export const NgxEditorConfig = {
   spellcheck: true,
   minHeight: "150px",
   toolbar: [
-    ["bold", "underline",],
-    ["orderedList", "unorderedList", "horizontalLine" ],
+    ["bold", "underline","removeFormat"],
+    ["orderedList", "unorderedList",  "outdent", "indent"],
     ["undo", "redo"],
   ],
   placeholder: "What is on your mind?"
